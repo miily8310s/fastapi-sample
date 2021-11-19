afabe5aee1994c955a26ff160c759bb94ecf7374
miruoon <rillakerika@gmail.com>
2021-11-20 08:45:14 +0900

    Add parameter sample code

--- fastapi-basic/minimum-app/basic.py
+++ fastapi-basic/minimum-app/basic.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 app = FastAPI()
 
 # getメソッド
@@ -9,3 +10,19 @@ async def helloWorld():
 @app.get('/hogefuga')
 async def hogeFuga():
   return {'hoge': 'fuga'}
+
+## /{param}に入っているものはparams、それ以外はquery扱いとなる
+@app.get('/parameter/{path}')
+async def getParameter(
+    path: str, # params
+    query: int,  # query（必須）
+    default_none: Optional[str] = None # query（任意）
+  ):
+  '''
+  ex) \n
+    /parameter/aaa/?query=1
+    return {"sample_text":"aaa, 1 default is None"} \n
+    /parameter/bbb?query=2&default_none=test
+    return {"sample_text":"bbb, 2 default is test"}
+  '''
+  return {"sample_text": f"{path}, {query} default is {default_none}"}
