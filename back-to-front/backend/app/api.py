07c5698780d4c7e5668a2d37848d062164bbbb05
miruoon <rillakerika@gmail.com>
2021-11-22 15:11:16 +0900

    feat:sample-app データ取得APIを追加

--- back-to-front/backend/app/api.py
+++ back-to-front/backend/app/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from . import models, schemas
@@ -38,3 +38,15 @@ def create_article(request: schemas.Article, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_article)
     return new_article
+
+@app.get('/get-all-articles')
+def get_all_articles(db: Session = Depends(get_db)):
+    articles = db.query(models.Article).all()
+    return articles
+
+@app.get('/get-article/{id}')
+def get_article(id: int, db: Session = Depends(get_db)):
+    article = db.query(models.Article).filter(models.Article.id == id).first()
+    if not article:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Article with the id {id} is not found')
+    return article
