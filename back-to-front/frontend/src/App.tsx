895bba55cf61d621bc41d145b6ec989a251f08e3
miruoon <rillakerika@gmail.com>
2021-11-23 13:46:44 +0900

    feat:sample-app Buttonコンポーネントを追加

--- back-to-front/frontend/src/App.tsx
+++ back-to-front/frontend/src/App.tsx
@@ -1,18 +1,30 @@
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ErrorBoundary } from "react-error-boundary";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "routes";
+import { Button } from "components/Elements/Button";
 
 const client = new QueryClient();
+const ErrorFallback = () => {
+  return (
+    <div>
+      <h2>問題が発生しました</h2>
+      <Button onClick={() => window.location.reload()}>再ロードする</Button>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <QueryClientProvider client={client}>
-      <ReactQueryDevtools />
-      <Router>
-        <AppRoutes />
-      </Router>
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <QueryClientProvider client={client}>
+        <ReactQueryDevtools />
+        <Router>
+          <AppRoutes />
+        </Router>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
